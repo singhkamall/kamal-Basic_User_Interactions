03d771f485a81026ee6b162d68487dbdb612e78f
Student <student@SETASs-Mac-mini.local>
2017-09-13 20:48:41 -0400

    initial commit

---
+++ Kamal-Basic_User_Interaction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kamal-Basic_User_Interaction
+//
+//  Created by Student on 2017-09-13.
+//  Copyright © 2017 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
